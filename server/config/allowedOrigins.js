5beb22434c648daf7e8843417d371cd96048af9b
Phil <philwekullo@gmail.com>
2024-09-24 16:14:35 +0300

    Update allowedOrigins in server/config/allowedOrigins.js

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 export const allowedOrigins = [
-    'http://localhost:3000/*',
-    'https://foster-care-woad.vercel.app/*'
+    'http://localhost:3000',
+    'https://foster-care-woad.vercel.app'
 ]
 
 // module.exports = allowedOrigins
