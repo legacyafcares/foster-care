6d1e39900005aec866909873f5ec6b307393aed1
Phil <philwekullo@gmail.com>
2025-02-16 23:00:45 +0300

    Remove hero bg gradient and update layout

--- client/app/components/Hero.tsx
+++ client/app/components/Hero.tsx
@@ -17,12 +17,12 @@ const Hero = () => {
       />
 
       {/* Overlay with a dark gradient */}
-      <div className="absolute top-0 left-0 size-[100%]" />
-      <div className="absolute md:top-[200px] xl:top-[250px] md:ml-[100px] lg:ml-[600px] top-[150px] flex flex-col items-start justify-center p-4 md:w-3/4 lg:w-1/2 lg:right-0 text-white">
-        <h1 className="p-3 lg:text-6xl text-4xl font-extrabold">
+      <div className="absolute top-0 left-0 size-[100%] lg:bg-gradient-to-l lg:from-black/30 lg:to-black/10 bg-gradient-to-b from-black/10 to-black/30" />
+      <div className="absolute md:top-[200px] xl:top-[250px] md:ml-[100px] lg:ml-[200px] top-[150px] flex flex-col items-center justify-center p-4 md:w-3/4  text-white">
+        <h1 className="p-3 lg:text-7xl text-4xl font-extrabold text-center">
           Compassionate care for your loved ones
         </h1>
-        <p className="lg:text-xl p-4 font-afacad">
+        <p className="lg:text-2xl p-4 font-afacad text-center">
           At our foster care service, we prioritize the well-being and happiness
           of the elderly. Join us in creating a nurturing environment that feels
           like home.
